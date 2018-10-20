c8d822bb42171e9b87390c4fe4b7443e8b7fc7da
Nutdanai <32787607+basbev@users.noreply.github.com>
2018-10-20 18:07:30 +0700

    1.01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,100 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+  <div class='hello'>
+    <img src = "/static/page_1.jpg" width="300">
+    <div>
+      <input type="text" v-model="name" placeholder="ชื่อร้านอาหาร">
+      <input type="text" v-model="tel" placeholder="เบอร์">
+      <button @click="insertTofoodcenter(tel, name)">Add</button>
+    </div>
+
+    <hr>
+
+    <ul :key="key" v-for="(foodcenter, key) in foodcenters">
+      <li v-if="updateKey === key">
+        <input type="text" v-model="updateName" placeholder="NAME">
+        <input type="text" v-model="updateTel" placeholder="TEL">
+        <button @click="updatefoodcenter(updateTel, updateName)">Save</button>
       </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
+      <li v-else>
+        {{foodcenter.name}} : {{foodcenter.tel}}
+        <button @click="setUpdatefoodcenter(key, foodcenter)">Update</button>
+        <button @click="deletefoodcenter(key)">Delete</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import * as firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyDIfmWOTTfPFqBIN7h3J3-yt-Ey2hpYnOk',
+  authDomain: 'foodcenter-23d67.firebaseapp.com',
+  databaseURL: 'https://foodcenter-23d67.firebaseio.com',
+  projectId: 'foodcenter-23d67',
+  storageBucket: 'foodcenter-23d67.appspot.com',
+  messagingSenderId: '4604297708'
+}
+firebase.initializeApp(config)
+var database = firebase.database()
+var foodcenterRef = database.ref('/foodcenter')
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      foodcenters: {},
+      tel: '',
+      name: '',
+      updateTel: '',
+      updateName: '',
+      updateKey: ''
     }
+  },
+  methods: {
+    insertTofoodcenter (tel, name) {
+      let data = {
+        tel: tel,
+        name: name
+      }
+      foodcenterRef.push(data)
+      this.tel = ''
+      this.name = ''
+    },
+    setUpdatefoodcenter (key, foodcenter) {
+      this.updateKey = key
+      this.updateTel = foodcenter.tel
+      this.updateName = foodcenter.name
+    },
+    updatefoodcenter (tel, name) {
+      foodcenterRef.child(this.updateKey).update({
+        tel: tel,
+        name: name
+      })
+      this.updateKey = ''
+      this.updateTel = ''
+      this.updateName = ''
+    },
+    deletefoodcenter (key) {
+      foodcenterRef.child(key).remove()
+    }
+  },
+  mounted () {
+    foodcenterRef.on('value', (snapshot) => {
+      this.foodcenters = snapshot.val()
+    })
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
