d44c19062cfb3b240230774024fbd04e3e93f3a3
Nutdanai <5806021630070@github.com>
2018-10-23 00:24:52 +0700

    Add Srcipt

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,47 @@
 <template>
-    <nav>
-        <div class="">
-        <div class="container">
-            <router-link to="/"
-            class="brand-logo center">FoodCenter</router-link>
-            <ul class="right">
-        <li><a class="waves-effect waves-light btn">Login</a></li>
-        <li><a class="waves-effect waves-light btn">Register</a></li>
-        <li><a class="waves-effect waves-light btn"><router-link to="/foodcenter">FoodCenter</router-link></a></li>
-      </ul>
-        </div>
+<nav class="navbar is-black">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <img src="/static/page_1.jpg" width="112" height="28">
+    </a>
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
     </div>
- </nav>
+  </div>
+
+  <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/">
+        Home
+      </a>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+              <span class="icon">
+                <i class="fab fa-twitter"></i>
+              </span>
+              <span>
+                Tweet
+              </span>
+            </a>
+          </p>
+          <p class="control">
+            <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip">
+              <span class="icon">
+                <i class="fas fa-download"></i>
+              </span>
+              <span>Download</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
 </template>
