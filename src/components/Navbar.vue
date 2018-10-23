b0abc0198582c722152ab78bc187d5c4cb68a139
Nutdanai <5806021630070@github.com>
2018-10-23 16:16:37 +0700

    E$

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,12 @@
   <div id="navbarExampleTransparentExample" class="navbar-menu">
     <div class="navbar-start">
       <a class="navbar-item" href="/">
-        Home
+        <span class="icon">
+                <i class="fas fa-home"></i>
+              </span>
+              <span>
+                Home
+              </span>
       </a>
     </div>
 
@@ -22,21 +27,15 @@
       <div class="navbar-item">
         <div class="field is-grouped">
           <p class="control">
-            <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
-              <span class="icon">
-                <i class="fab fa-twitter"></i>
-              </span>
+            <a class="bd-tw-button button" href="/">
               <span>
-                Tweet
+                Login
               </span>
             </a>
           </p>
           <p class="control">
-            <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip">
-              <span class="icon">
-                <i class="fas fa-download"></i>
-              </span>
-              <span>Download</span>
+            <a class="button is-primary" href="/">
+              <span>Sign Up</span>
             </a>
           </p>
         </div>
--- src/components/login.vue
+++ src/components/login.vue
@@ -4,7 +4,6 @@
 <div class="row">
 <div class="col s12 m8 offset-m2">
 <div class="login card-panel red lighten-5 white-text center">
-<span class="blue-text text-darken-2"><h4>FoodCenter</h4></span>
 <form>
 <div class="input-field">
 <input type="text" id="user">
@@ -15,11 +14,59 @@
 <label for="password">Password</label>
 </div>
 <button class="btn btn-large grey lighten-4 black-text">Login To FoodCenter</button>
+            <a class="bd-tw-button button" data-social-network="Facebook" data-social-action="facebook" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+              <span class="icon">
+                <i class="fab fa-facebook"></i>
+              </span>
+              <span>
+                facebook
+              </span>
+            </a>
 </form>
 </div>
 </div>
 </div>
 </div>
+<section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Login</h3>
+                    <p class="subtitle has-text-grey">Please login to FoodCenter.</p>
+                    <div class="box">
+                        <figure class="avatar">
+                            <img src="https://placehold.it/128x128">
+                        </figure>
+                        <form>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="email" placeholder="Your Email" autofocus="">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="password" placeholder="Your Password">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="checkbox">
+                  <input type="checkbox">
+                  Remember me
+                </label>
+                            </div>
+                            <button class="button is-block is-info is-large is-fullwidth">Login</button>
+                        </form>
+                    </div>
+                    <p class="has-text-grey">
+                        <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                        <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                        <a href="../">Need Help?</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 </template>
 
@@ -33,3 +80,40 @@ export default {
   }
 }
 </script>
+<style>
+html,body {
+  font-family: 'Open Sans', serif;
+  font-size: 14px;
+  font-weight: 300;
+}
+.hero.is-success {
+  background: #F2F6FA;
+}
+.hero .nav, .hero.is-success .nav {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.box {
+  margin-top: 5rem;
+}
+.avatar {
+  margin-top: -70px;
+  padding-bottom: 20px;
+}
+.avatar img {
+  padding: 5px;
+  background: #fff;
+  border-radius: 50%;
+  -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+  box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+}
+input {
+  font-weight: 300;
+}
+p {
+  font-weight: 700;
+}
+p.subtitle {
+  padding-top: 1rem;
+}
+</style>
