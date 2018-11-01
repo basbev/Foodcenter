45dd5310cabeff6eb5f29fbac1585995138331d4
Nutdanai <5806021630070@github.com>
2018-11-02 00:29:21 +0700

    Update src/components/shop.vue

---
+++ src/components/shop.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <h1>This Shop is {{ this.selectShop }} </h1>
+    </div>
+</template>
+<script>
+export default {
+  name: 'shop',
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  },
+  computed: {
+    selectShop () {
+      return this.$store.getters.selectShop
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
