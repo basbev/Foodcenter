b75c5f37ea675b17f5b4110f59f8e7556aa7fad7
Nutdanai <5806021630070@github.com>
2018-12-14 01:09:00 +0700

    Update Reportsell.vue

--- src/components/Reportsell.vue
+++ src/components/Reportsell.vue
@@ -5,6 +5,9 @@
         <button @click="getDataDay(genFunction)">รายวัน</button>
         <button @click="getDataMonth(genFunction)">รายเดือน</button>
         <button @click="getDataYear(genFunction)">รายปี</button>
+        <button @click="getDataFirebase(getvalue)">ทดสอบ</button>
+        <button @click="ShowGraph(getvalue)">กราฟ</button>
+
     <div id="chart-container">
     </div>
 </div>
@@ -17,12 +20,13 @@ export default {
   name: 'Reportsell',
   data: function () {
     return {
-      getvalue: ''
+      getvalue: [],
+      datavshow: ''
     }
   },
   methods: {
     getDataDay: function (genFunction) {
-      var ref = firebase.database().ref('foodcenter/report/' + this.selectShop + '/day')
+      var ref = firebase.database().ref('foodcenter/report/' + 'ป้าสมบูรณ์' + '/day')
       ref.once('value').then(function (snapshot) {
         genFunction(snapshot.val())
       })
@@ -49,6 +53,7 @@ export default {
           value: data[i]['value']
         })
       }
+      console.log(cdata)
       var firebaseChart = new FusionCharts({
         type: 'column2d',
         renderAt: 'chart-container',
@@ -104,6 +109,74 @@ export default {
     },
     exportPdf () {
       window.print()
+    },
+    getDataFirebase (getvalue) {
+      alert(`Pls Wait`)
+      var ref = firebase.database().ref('foodcenter/report/ป้าสมบูรณ์/day')
+      ref.once('value', snap => {
+        var data = []
+        snap.forEach(ss => {
+          var item = ss.val()
+          data.push(item)
+        })
+        this.getvalue = data
+        console.log(this.getvalue)
+      })
+    },
+    ShowGraph: function (getvalue) {
+      console.log(this.getvalue)
+      var firebaseChart = new FusionCharts({
+        type: 'column2d',
+        renderAt: 'chart-container',
+        width: '650',
+        height: '400',
+        dataFormat: 'json',
+        dataSource: {
+          'chart': {
+            'caption': 'Report',
+            'subCaption': 'Days{br}Foodcenter Inc.',
+            'subCaptionFontBold': '0',
+            'captionFontSize': '20',
+            'subCaptionFontSize': '17',
+            'captionPadding': '15',
+            'captionFontColor': '#8C8C8C',
+            'baseFontSize': '14',
+            'baseFont': 'Barlow',
+            'canvasBgAlpha': '0',
+            'bgColor': '#FFFFFF',
+            'bgAlpha': '100',
+            'showBorder': '0',
+            'showCanvasBorder': '0',
+            'showPlotBorder': '0',
+            'showAlternateHGridColor': '0',
+            'usePlotGradientColor': '0',
+            'paletteColors': '#6AC1A5',
+            'showValues': '0',
+            'divLineAlpha': '5',
+            'showAxisLines': '1',
+            'drawAnchors': '0',
+            'xAxisLineColor': '#8C8C8C',
+            'xAxisLineThickness': '0.7',
+            'xAxisLineAlpha': '50',
+            'yAxisLineColor': '#8C8C8C',
+            'yAxisLineThickness': '0.7',
+            'yAxisLineAlpha': '50',
+            'baseFontColor': '#8C8C8C',
+            'toolTipBgColor': '#FA8D67',
+            'toolTipPadding': '10',
+            'toolTipColor': '#FFFFFF',
+            'toolTipBorderRadius': '3',
+            'toolTipBorderAlpha': '0',
+            'drawCrossLine': '1',
+            'crossLineColor': '#8C8C8C',
+            'crossLineAlpha': '60',
+            'crossLineThickness': '0.7',
+            'alignCaptionWithCanvas': '1'
+          },
+          'data': getvalue
+        }
+      })
+      firebaseChart.render()
     }
   },
   mounted () {
